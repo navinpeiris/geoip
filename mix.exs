38f1e7e9b9590bb9850116faf56bd18ce703130f
Navin Peiris <navinpeiris@gmail.com>
2018-09-05 12:24:23 +0530

    Bump version to `0.2.1`

--- mix.exs
+++ mix.exs
@@ -2,26 +2,29 @@ defmodule GeoIP.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :geoip,
-     version: "0.2.0",
-     name: "GeoIP",
-     description: "Lookup the geo location for a given IP address, hostname or Plug.Conn instance",
-     package: package(),
-     source_url: "https://github.com/navinpeiris/geoip",
-     homepage_url: "https://github.com/navinpeiris/geoip",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps(),
-     docs: [extras: ["README.md"]]]
+    [
+      app: :geoip,
+      version: "0.2.1",
+      name: "GeoIP",
+      description:
+        "Lookup the geo location for a given IP address, hostname or Plug.Conn instance",
+      package: package(),
+      source_url: "https://github.com/navinpeiris/geoip",
+      homepage_url: "https://github.com/navinpeiris/geoip",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      docs: [extras: ["README.md"]]
+    ]
   end
 
   def application do
-    [applications: [:logger, :httpoison, :cachex],
-     mod: {GeoIP, []},
-     env: [use_https: true,
-           cache: true,
-           cache_ttl_secs: 3600]]
+    [
+      applications: [:logger, :httpoison, :cachex],
+      mod: {GeoIP, []},
+      env: [use_https: true, cache: true, cache_ttl_secs: 3600]
+    ]
   end
 
   defp deps do
@@ -29,17 +32,17 @@ defmodule GeoIP.Mixfile do
       {:httpoison, "~> 1.0"},
       {:poison, "~> 2.0 or ~> 3.0"},
       {:cachex, "~> 3.0"},
-
       {:ex_doc, ">= 0.0.0", only: :dev},
-
       {:mock, "~> 0.3", only: :test}
     ]
   end
 
   defp package do
-    [files: ["lib", "mix.exs", "README*", "LICENSE*"],
-     maintainers: ["Navin Peiris"],
-     licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/navinpeiris/geoip"}]
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Navin Peiris"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/navinpeiris/geoip"}
+    ]
   end
 end
