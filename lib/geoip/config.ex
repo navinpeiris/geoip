406c86cb9fd0b814dc99f3b1098feb6237a6b570
Stanislav Versilov <stas.versilov@gmail.com>
2019-10-01 15:21:25 +0400

    Add :extra_params config option

--- lib/geoip/config.ex
+++ lib/geoip/config.ex
@@ -17,7 +17,7 @@ defmodule GeoIP.Config do
 
   def default_test_result, do: get(:default_test_result)
   
-  def extra_params, do: (get(:extra_params) || []) |> URI.encode_query()
+  def extra_params, do: if(get(:extra_params), do: "&" <> URI.encode_query(get(:extra_params)), else: "")
 
   defp get!(key) do
     get(key) || raise(ArgumentError, "Missing configuration parameter `:#{key}`")
